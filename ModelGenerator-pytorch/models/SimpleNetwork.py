eda20231d0375e2e36f3622a6431ca30a3139275
Alexander Pacha <alexander.pacha@gmail.com>
2019-01-25 17:38:40 +0100

    Adding a simple model that mirrors the SimpleConfiguration from the Tensorflow/Keras implementation.

---
+++ ModelGenerator-pytorch/models/SimpleNetwork.py
@@ -0,0 +1,33 @@
+from torch import nn
+import torch.nn.functional as F
+
+
+class SimpleNetwork(nn.Module):
+    """ A rudimentary configuration for starting """
+
+    def __init__(self):
+        super(SimpleNetwork, self).__init__()
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv1 = nn.Conv2d(3, 64, 7, padding=1)
+        self.conv2 = nn.Conv2d(64, 96, 3, padding=1)
+        self.conv3 = nn.Conv2d(96, 128, 3, padding=1)
+        self.conv4 = nn.Conv2d(128, 192, 3, padding=1)
+        self.dropout = nn.Dropout(0.5)
+        self.fc1 = nn.Linear(192 * 7 * 7, 64)
+        self.fc2 = nn.Linear(64, 32)
+        self.fc3 = nn.Linear(32, 2)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv3(x)))
+        x = self.pool(F.relu(self.conv4(x)))
+        x = x.view(-1, 192 * 7 * 7)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+    def name(self) -> str:
+        """ Returns the name of this configuration """
+        return "simple"
