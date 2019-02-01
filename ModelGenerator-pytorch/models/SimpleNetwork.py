5c2070406238eae4e6b9e105cb2d876debdbf3ef
Alexander Pacha <alexander.pacha@gmail.com>
2019-02-01 14:28:38 +0100

    Replacing manual calculation of size in flattening-layer with automatic computation

--- ModelGenerator-pytorch/models/SimpleNetwork.py
+++ ModelGenerator-pytorch/models/SimpleNetwork.py
@@ -22,7 +22,7 @@ class SimpleNetwork(nn.Module):
         x = self.pool(F.relu(self.conv2(x)))
         x = self.pool(F.relu(self.conv3(x)))
         x = self.pool(F.relu(self.conv4(x)))
-        x = x.view(-1, 192 * 7 * 7)
+        x = x.view(x.size(0), -1)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
